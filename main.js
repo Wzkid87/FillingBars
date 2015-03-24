5077e542eede3e5f34c04ba3668942e5aa856e16
Wzkid87 <Wzkid87@gmail.com>
2015-03-24 14:04:15 -0500

    Update main.js

--- main.js
+++ main.js
@@ -31,10 +31,10 @@ $('#buyIncr').on('click',function() {
 });
 
 window.setInterval(function() {
-  curr1+=fillers;
-  if (curr1 >= 7) {
-    curr2 += 1;
-    curr1 -= 7;};
+  curr1+=fillers/10;
+  if (curr1 > 7) {
+    curr2 += Math.floor(curr1/7);
+    curr1 = curr1 % 7;};
   if (curr2 == 49) {
     curr3 += 1;
     curr2 = 0;};
