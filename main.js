95c3a457ddfc46ba709dec5364fcd2f85447ee08
Wzkid87 <Wzkid87@gmail.com>
2015-03-24 13:54:47 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var curr3 = 0;
 var curr4 = 0;
 var curr5 = 0;
 var incr = 1;
+var fillers = 0;
 
 var bar1 = document.getElementById('firstBar');
 var bar2 = document.getElementById('secondBar');
@@ -13,16 +14,32 @@ var bar5 = document.getElementById('fifthbar');
 
 $('#fill').on('click', function () {
   curr1 = curr1 + incr;
-  if (curr1 == 10) {
+};
+
+$('#buyFiller').on('click',function() {
+  fillers++;
+  curr2-=fillerCost;
+  fillerCost++;
+});
+
+$('#buyIncr').on('click',function() {
+  incr++;
+  curr2-=incrCost;
+  incrCost++;
+});
+
+window.setInterval(function() {
+  curr1+=fillers;
+  if (curr1 >= 7) {
     curr2 += 1;
     curr1 = 0;};
-  if (curr2 == 100) {
+  if (curr2 == 49) {
     curr3 += 1;
     curr2 = 0;};
-  if (curr3 == 1000) {
+  if (curr3 == 343) {
     curr4 += 1;
     curr3 = 0;};
-  if (curr4 == 10000) {
+  if (curr4 == 2401) {
     curr5 += 1;
     curr4 = 0;};
     
@@ -31,5 +48,5 @@ $('#fill').on('click', function () {
     bar3.value = curr3;
     bar4.value = curr4;
     bar5.value = curr5;
-  });
+  },10);
   
